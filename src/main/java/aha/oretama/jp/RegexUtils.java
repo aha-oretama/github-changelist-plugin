93200c01129889f988645bce6ed10cce6a92e944
aha-oretama <sekine_y_529@msn.com>
2017-04-17 17:29:57 +0900

    Refactoring RegexUtils and make test case

--- src/main/java/aha/oretama/jp/RegexUtils.java
+++ src/main/java/aha/oretama/jp/RegexUtils.java
@@ -1,7 +1,32 @@
 package aha.oretama.jp;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author sekineyasufumi on 2017/04/17.
  */
 public class RegexUtils {
+
+    public static List<String> createRegexps(List<String> changelist, String regex, String testTargetRegex) {
+        Pattern pattern = Pattern.compile(regex);
+
+        List<String> regexps = new ArrayList<>();
+        for (String change : changelist) {
+            Matcher matcher = pattern.matcher(change);
+
+            if (matcher.find() && matcher.groupCount() >= 1) {
+                for (int i = 1; i <= matcher.groupCount(); i++) {
+                    if(matcher.group(i) != null) {
+                        regexps.add(testTargetRegex.replace("$" + i, matcher.group(i)));
+                    }
+                }
+            }
+        }
+
+        return regexps;
+    }
+
 }
--- src/test/java/aha/oretama/jp/RegexUtilsTest.java
+++ src/test/java/aha/oretama/jp/RegexUtilsTest.java
@@ -1,8 +1,37 @@
-import static org.junit.Assert.*;
+package aha.oretama.jp;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.Assert.*;
 /**
  * @author sekineyasufumi on 2017/04/17.
  */
 public class RegexUtilsTest {
+
+    private static final List changelist = new ArrayList();
+
+    // Default
+    private String regex = "([^/]*?)(Test)?\\..*$";
+    // Default
+    private String testTargetRegex = "$1Test";
+
+
+    static {
+        changelist.add("src/java/util.java");
+        changelist.add("src/java/groovyUtil.groovy");
+        changelist.add("src/java/testUtilTest.groovy");
+    }
+
+    @Test
+    public void createRegexps() throws Exception {
+
+        List resultList = RegexUtils.createRegexps(changelist, regex, testTargetRegex);
+
+        assertEquals("utilTest",resultList.get(0));
+        assertEquals("groovyUtilTest",resultList.get(1));
+        assertEquals("testUtilTest",resultList.get(2));
+    }
 
 }
